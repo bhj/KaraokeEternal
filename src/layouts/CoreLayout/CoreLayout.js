18e6b2fa029561d8d17529da9b33f70749232fe0
Brandon Jones <brandon@radroot.com>
2016-05-20 01:49:14 -0400

    Simplify layout

--- src/layouts/CoreLayout/CoreLayout.js
+++ src/layouts/CoreLayout/CoreLayout.js
@@ -4,7 +4,7 @@ import classes from './CoreLayout.scss'
 import '../../styles/core.scss'
 
 export const CoreLayout = ({ children }) => (
-  <div className='container text-center'>
+  <div>
     <Header />
     <div className={classes.mainContainer}>
       {children}
