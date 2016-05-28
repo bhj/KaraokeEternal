7b7653c6873f8143ccfea0ae5e802e996a46163d
Brandon Jones <brandon@radroot.com>
2016-05-28 02:18:47 -0400

    Load ArtistsContainer synchronously

--- src/routes/Library/routes/Artists/index.js
+++ src/routes/Library/routes/Artists/index.js
@@ -1,24 +1,6 @@
-import { injectReducer } from 'store/reducers'
+import ArtistsContainer from './containers/ArtistsContainer'
 
 export default (store) => ({
   path: 'artists',
-  /*  Async getComponent is only invoked when route matches   */
-  getComponent (nextState, cb) {
-    /*  Webpack - use 'require.ensure' to create a split point
-        and embed an async module loader (jsonp) when bundling   */
-    require.ensure([], (require) => {
-      /*  Webpack - use require callback to define
-          dependencies for bundling   */
-      const Artists = require('./containers/ArtistsContainer').default
-      const reducer = require('./modules/artists').default
-
-      /*  Add the reducer to the store on key 'counter'  */
-      injectReducer(store, { key: 'artists', reducer })
-
-      /*  Return getComponent   */
-      cb(null, Artists)
-
-    /* Webpack named bundle   */
-  }, 'artists')
-  }
+  component: ArtistsContainer
 })
