3cba6d618fae17c9ab249bfc1b55c53a98a411c1
Brandon Jones <brandon@radroot.com>
2016-06-28 16:37:04 -0400

    Don't load queue on route change (updates come in over socket)

--- src/routes/Queue/index.js
+++ src/routes/Queue/index.js
@@ -1,16 +1,11 @@
-import { requireAuth } from 'components/requireAuth'
 import QueueContainer from './containers/QueueContainer'
-import { fetchQueue } from './modules/queue'
+import { requireAuth } from 'components/requireAuth'
 
 // route definition
 export default function(store){
   return {
     path: 'queue',
     getComponent (nextState, cb) {
-      if (!store.getState().queue.result.length && store.getState().account.user) {
-        store.dispatch(fetchQueue(store.getState().account.user.roomId))
-      }
-
       cb(null, requireAuth(QueueContainer))
     }
   }
