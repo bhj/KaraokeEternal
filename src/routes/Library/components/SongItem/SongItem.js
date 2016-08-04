fed6d46619a8bd690d39eb3f6f0ec7616fa8c22f
Brandon Jones <brandon@radroot.com>
2016-08-04 01:16:53 -0400

    Simplify songItem display and show duration

--- src/routes/Library/components/SongItem/SongItem.js
+++ src/routes/Library/components/SongItem/SongItem.js
@@ -4,19 +4,15 @@ import classes from './SongItem.css'
 export const SongItem = (props) => (
   <div onClick={props.onSelectSong} className={classes.container + (props.isQueued ? ' ' + classes.isQueued : '')}>
     <div className={classes.star}>
-      <i className='material-icons md-48 md-dark md-inactive'>
+      <i className='material-icons md-48 md-inactive'>
         {props.isStarred ? 'star' : 'star_border'}
       </i>
     </div>
 
-    <div className={classes.title}>{props.title} ({props.provider})</div>
+    <div className={classes.title}>{props.title}</div>
 
     <div className={classes.stats}>
-      <i className='material-icons md-18 md-dark md-inactive'>play_arrow</i>
-      {props.plays}
-
-      <i className='material-icons md-18 md-dark md-inactive'>star</i>
-      0
+      {props.duration}
     </div>
   </div>
 )
