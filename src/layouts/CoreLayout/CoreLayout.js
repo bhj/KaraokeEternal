ebf38b274f03e32b3013fa3f9bad47585a635fb9
Brandon Jones <brandon@radroot.com>
2016-11-19 16:16:57 -0500

    Style tweaks

--- src/layouts/CoreLayout/CoreLayout.js
+++ src/layouts/CoreLayout/CoreLayout.js
@@ -1,18 +1,14 @@
 import React from 'react'
 import './CoreLayout.scss'
-import '../../styles/core.scss'
-import '../../styles/nomodule/material-ui.scss'
+import 'styles/core.scss'
+import 'styles/material-ui.scss'
 
 export const CoreLayout = ({ children }) => {
-    return (
-      <div style={{height: '100%'}}>
-        {children}
-      </div>
-    )
-}
-
-CoreLayout.propTypes = {
-  children : React.PropTypes.element
+  return (
+    <div style={{height: '100%'}}>
+      {children}
+    </div>
+  )
 }
 
 export default CoreLayout
