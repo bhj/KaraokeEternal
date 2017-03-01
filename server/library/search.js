9ec7c04f00fe1f912ec66d3bbc466f385592f397
Brandon Jones <brandon@radroot.com>
2017-03-01 15:57:38 -0500

    Add 'squel' dep (query builder) and use in library searches to fix shit code

--- server/library/search.js
+++ server/library/search.js
@@ -1,48 +1,43 @@
 const db = require('sqlite')
+const squel = require('squel')
 const debug = require('debug')
 const log = debug('app:library:search')
 
 async function searchLibrary(params = {}) {
-  let sql, res
   let songs = {
     result: [],
     entities: {}
   }
-  let where = []
+
+  const q = squel.select().from('songs')
 
   Object.keys(params).map(key => {
     if (key === 'meta' && typeof params.meta === 'object') {
       Object.keys(params.meta).map(i => {
-        let val = params.meta[i]
-        if (typeof val === 'string') val = "'"+val+"'" // ugh
-        where.push(`json_extract(provider_json, '$.${i}') = ${val}`)
+        q.where(`json_extract(provider_json, '$.${i}') = ?`, params.meta[i])
       })
-      return
+    } else {
+      q.where(`${key} = ?`, params[key])
     }
-
-    where.push(`${key} = '${params[key]}'`)
   })
-
-  sql = 'SELECT * FROM songs'
-  if (where.length) sql += ' WHERE ' + where.join(' AND ')
-  sql += ' ORDER BY title'
 
   // get songs
   try {
-    log(sql)
-    res = await db.all(sql)
+    log(q.toString())
+    const { text, values } = q.toParam()
+    let res = await db.all(text, values)
+
+    // normalize results
+    res.forEach(function(row){
+      songs.result.push(row.songId)
+      songs.entities[row.songId] = row
+    })
   } catch(err) {
-    console.log(err)
+    log(err)
     return Promise.reject(err)
   }
 
-  // normalize results
-  res.forEach(function(row){
-    songs.result.push(row.songId)
-    songs.entities[row.songId] = row
-  })
-
-  log('%s result(s)', songs.result.length)
+  log('%s result' + (songs.result.length === 1 ? '' : 's'), songs.result.length)
   return songs
 }
 
