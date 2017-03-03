c4f513884c78b3a03174539cfffd0d1cdbcd07bd
Brandon Jones <brandon@radroot.com>
2017-03-03 12:16:21 -0500

    Improve comments in kfconfig-default.js

--- server/library/parseArtistTitle/kfconfig-default.js
+++ server/library/parseArtistTitle/kfconfig-default.js
@@ -1,17 +1,15 @@
 // karaoke-forever string to artist/title parser defaults
 module.exports = {
-  // used to split into artist/song (regex or string)
+  // regex or string; artist/song get split around this match (default='-')
   delimiter: '-',
-  // whether artist is on left side of delimiter (default=true)
-  // @todo: invert for RTL languages
+  // bool; whether artist is on left side of delimiter (default=true)
   artistFirst: true,
-  // force Artist to this string
+  // string; override Artist for songs in this file's scope (default='')
   artist: '',
 
-  // replace options
-  // --------------------
-  // these can use regex or string for the search;
-  // repl='' is assumed or specify an array, e.g. [search, repl]
+  // each stage is configured with regexes and/or strings and
+  // simply removes matches by default (repl=''); use an array
+  // to pass a replacement param/string, e.g. [find, repl]
   replacements: {
     // applied to input string before split to Artist/Title
     preSplit: [
