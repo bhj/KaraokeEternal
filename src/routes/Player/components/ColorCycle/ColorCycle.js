c9b3980f84c397619494f1ec3cf0784980aed3ec
Brandon Jones <brandon@radroot.com>
2017-03-17 17:45:24 -0400

    Add chroma.js dep; use JS to handle color cycling instead of SCSS

--- src/routes/Player/components/ColorCycle/ColorCycle.js
+++ src/routes/Player/components/ColorCycle/ColorCycle.js
@@ -1,30 +1,72 @@
 import React, { PropTypes } from 'react'
-import './ColorCycle.scss'
+import chroma from 'chroma-js'
+import classes from './ColorCycle.css'
 
-const ColorCycle = (props) => {
-  // add <span>s around each character
-  const chars = props.title.split('')
-  const text = chars.map(char =>
-    <span key={uniqueId()}>{char}</span>
-  )
+const colorNames = ['red','purple','blue','green', 'yellow','orange','red']
+const cycleSpeed = 50 // ms
 
-  return (
-    <div style={{display: 'flex', height: '100%'}}>
-      <div className="anim-text-flow">
+class ColorCycle extends React.Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+  }
+
+  state = {
+    offset: 0,
+  }
+
+  chars = []
+
+  render() {
+    const text = this.chars.map((char, i) => {
+      i += this.state.offset
+
+      if (i >= this.colors.length) {
+        i -= this.colors.length
+      }
+
+      return (
+        <span key={char+i} style={{color: this.colors[i]}}>
+          {char}
+        </span>
+      )
+    })
+
+    return (
+      <div className={classes.text}>
         {text}
       </div>
-    </div>
-  )
-}
+    )
+  }
 
-ColorCycle.PropTypes = {
-  title: PropTypes.string.isRequired,
+  componentDidUpdate(prevProps) {
+    if (prevProps.title !== this.props.title) {
+      this.updateTitle()
+    }
+  }
+
+  componentDidMount() {
+    this.updateTitle()
+
+    this.timer = setInterval(() => {
+      this.setState({
+        offset: this.state.offset < this.colors.length ? ++this.state.offset : 0
+      })
+    }, cycleSpeed)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer)
+  }
+
+  updateTitle = () => {
+    // array of chars
+    this.chars = this.props.title.split('')
+
+    // array of hex colors spread throughout colorNames
+    this.colors = chroma.scale(colorNames)
+      .mode('lab')
+      .colors(this.chars.length * 10)
+  }
 }
 
 export default ColorCycle
-
-// creates a string that can be used for dynamic id attributes
-// http://www.frontcoded.com/javascript-create-unique-ids.html
-function uniqueId() {
-  return 'id-' + Math.random().toString(36).substr(2, 16)
-}
