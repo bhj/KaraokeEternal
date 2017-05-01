03354413cb3a59163301f5159ebf8c5cc18772c3
Brandon Jones <brandon@radroot.com>
2017-05-01 16:03:45 -0400

    YouTube player improvements and fixes

--- src/providers/youtube/YouTubePlayer/YouTubePlayer.js
+++ src/providers/youtube/YouTubePlayer/YouTubePlayer.js
@@ -4,11 +4,11 @@ import YouTube from 'react-youtube'
 
 class YouTubePlayer extends React.Component {
   static propTypes = {
+    queueItem: PropTypes.object.isRequired,
+    isPlaying: PropTypes.bool.isRequired,
     width: PropTypes.number.isRequired,
     height: PropTypes.number.isRequired,
     volume: PropTypes.number.isRequired,
-    isPlaying: PropTypes.bool.isRequired,
-    song: PropTypes.object.isRequired,
     onMediaError: PropTypes.func.isRequired, // action
     onMediaEnd: PropTypes.func.isRequired, // action
     onStatus: PropTypes.func.isRequired, // action
@@ -19,26 +19,21 @@ class YouTubePlayer extends React.Component {
   }
 
   componentDidUpdate (prevProps, prevState) {
-    if (prevProps.isPlaying !== this.props.isPlaying) {
-      this.updateIsPlaying()
-    }
-
-    if (prevProps.volume !== this.props.volume) {
-      this.setVolume(this.props.volume)
-    }
+    this.updateIsPlaying()
+    this.setVolume(this.props.volume)
   }
 
   render () {
-    if (!this.state.videoId) return null
+    const { width, height, queueItem } = this.props
+    if (!queueItem) return null
 
-    const { width, height, song } = this.props
     const opts = {
       width,
       height,
       // https://developers.google.com/youtube/player_parameters
       playerVars: {
-        autoplay: 1,
-        // controls: 0,
+        autoplay: 0,
+        controls: 0,
         disablekb: 1,
         fs: 0,
         rel: 0,
@@ -48,10 +43,10 @@ class YouTubePlayer extends React.Component {
     return (
       <div style={{ width, height }}>
         <YouTube
-          videoId={song.providerData.videoId}
+          videoId={queueItem.providerData.videoId}
           opts={opts}
           onReady={this.handleReady}
-          onStateChange={this.handleStatus}
+          onStateChange={this.handleStateChange}
           onError={this.handleError}
           onEnd={this.props.onMediaEnd}
         />
@@ -61,9 +56,40 @@ class YouTubePlayer extends React.Component {
 
   handleReady = (event) => {
     this.player = event.target
-    event.target.setVolume(this.props.volume * 100) // flakey using this.player?
-    clearInterval(this.statusTimer)
-    this.statusTimer = setInterval(this.handleStatus, 1000)
+    this.player.setVolume(this.props.volume * 100)
+
+    this.updateIsPlaying()
+  }
+
+  handleStateChange = (event) => {
+    switch (event.data) {
+      case 1:
+        // playing; need to poll position (vroom vroom!)
+        clearInterval(this.statusTimer)
+        this.statusTimer = setInterval(this.onStatus, 1000)
+        break
+      case 2:
+        // paused; cancel polling
+        clearInterval(this.statusTimer)
+        break
+      case 3:
+        // buffering; not much we can do here
+        return
+      case 5:
+        // cued; if we're playing, play!
+        this.updateIsPlaying()
+        break
+    }
+
+    // most codes trigger a status emit
+    this.onStatus()
+  }
+
+  onStatus = () => {
+    this.props.onStatus({
+      position: this.player.getCurrentTime(),
+      volume: this.player.getVolume() / 100,
+    })
   }
 
   handleError = (event) => {
@@ -74,20 +100,7 @@ class YouTubePlayer extends React.Component {
     this.player.setVolume(vol * 100)
   }
 
-  handleStatus = (event) => {
-    if (!event && !this.player) return
-    const player = event ? event.target : this.player
-
-    this.props.onStatus({
-      isPlaying: player.getPlayerState() === 1,
-      position: player.getCurrentTime(),
-      volume: player.getVolume() / 100,
-    })
-  }
-
   updateIsPlaying = () => {
-    if (!this.player) return
-
     if (this.props.isPlaying) {
       this.player.playVideo()
     } else {
