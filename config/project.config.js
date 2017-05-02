93c415a830fc0e4d4648dfbed00d917311eab4a6
Brandon Jones <brandon@radroot.com>
2017-05-02 01:13:32 -0400

    Use localhost for now

--- config/project.config.js
+++ config/project.config.js
@@ -24,7 +24,7 @@ const config = {
   // ----------------------------------
   // Server Configuration
   // ----------------------------------
-  server_host : ip.address(), // use string 'localhost' to prevent exposure on local network
+  server_host : 'localhost', // use string 'localhost' to prevent exposure on local network
   server_port : process.env.PORT || 3000,
 
   // ----------------------------------
