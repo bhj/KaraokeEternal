8b9364f051b0e4ad71feeb0714d515e30187d75b
Brandon Jones <brandon@radroot.com>
2017-05-07 16:50:19 -0400

    Add initial db schema

---
+++ server/lib/migrations/001-initial-schema.sql
@@ -0,0 +1,60 @@
+-- Up
+CREATE TABLE IF NOT EXISTS "artists" (
+  "artistId" integer PRIMARY KEY AUTOINCREMENT,
+  "name" text
+);
+
+CREATE TABLE IF NOT EXISTS "prefs" (
+  "domain" text,
+  "data" text
+);
+
+INSERT INTO prefs (domain,data) VALUES ('app.general','{"firstRun":true}');
+INSERT INTO prefs (domain,data) VALUES ('provider.cdg','{"enabled":true,"paths":[]}');
+INSERT INTO prefs (domain,data) VALUES ('provider.youtube','{"enabled":true,"channels":[],"apiKey":""}');
+
+CREATE TABLE IF NOT EXISTS "queue" (
+  "queueId" integer PRIMARY KEY AUTOINCREMENT,
+  "roomId" integer,
+  "songId" integer,
+  "userId" integer
+);
+
+CREATE TABLE IF NOT EXISTS "rooms" (
+  "roomId" integer PRIMARY KEY AUTOINCREMENT,
+  "status" text,
+  "name" text
+);
+
+CREATE TABLE IF NOT EXISTS "songs" (
+  "songId" integer PRIMARY KEY AUTOINCREMENT,
+  "artistId" integer,
+  "title" text,
+  "duration" integer,
+  "provider" text,
+  "providerData" text
+);
+
+CREATE TABLE IF NOT EXISTS "stars" (
+  "songId" integer NOT NULL,
+  "userId" integer NOT NULL
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS preventDupes ON "stars" ("songId", "userId");
+
+CREATE TABLE IF NOT EXISTS "users" (
+  "userId" integer PRIMARY KEY AUTOINCREMENT,
+  "email" text,
+  "password" text,
+  "name" text,
+  "isAdmin" integer
+);
+
+-- Down
+DROP TABLE artists;
+DROP TABLE prefs;
+DROP TABLE queue;
+DROP TABLE rooms;
+DROP TABLE songs;
+DROP TABLE stars;
+DROP TABLE users;
