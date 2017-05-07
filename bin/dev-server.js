405edba5fa576758f13d09032b349b81e4f83078
Brandon Jones <brandon@radroot.com>
2017-05-07 16:50:04 -0400

    Add database migration/initialization on startup

--- bin/dev-server.js
+++ bin/dev-server.js
@@ -3,12 +3,24 @@ const server = require('../server/main')
 const debug = require('debug')('app:bin:dev-server')
 const db = require('sqlite')
 
-// initialize database
-Promise.resolve()
-  .then(() => {
-    db.open(project.path_database, { Promise })
-    debug('Opened database file %s', project.path_database)
+// open/init/migrate database and start koa server listen()
+async function init () {
+  try {
+    debug('Opening database: %s', project.path_database)
+    await db.open(project.path_database)
+
+    debug('Running database migration checks')
+    await db.migrate({
+      // force: 'last',
+      migrationsPath: project.paths.server('lib/migrations'),
+    })
+
     server.listen(project.server_port)
     debug(`Server is now running at http://localhost:${project.server_port}.`)
-  })
-  .catch(err => debug(err.stack))
+  } catch (e) {
+    debug(e)
+    process.exit(1)
+  }
+}
+
+init()
