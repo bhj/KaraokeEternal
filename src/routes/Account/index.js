a2de8914d053a356c7b4a59cbcbd1a3df16c21d6
Brandon Jones <brandon@radroot.com>
2017-06-23 00:50:11 -0400

    Still need to fetch rooms on route load in some cases

--- src/routes/Account/index.js
+++ src/routes/Account/index.js
@@ -1,4 +1,5 @@
 import AccountContainer from './views/AccountContainer'
+import { fetchRooms } from 'store/modules/rooms'
 import { fetchPrefs } from 'store/modules/prefs'
 
 // route definition
@@ -6,6 +7,7 @@ export default function (store) {
   return {
     path: 'account',
     getComponent (nextState, cb) {
+      store.dispatch(fetchRooms())
       store.dispatch(fetchPrefs())
 
       cb(null, AccountContainer)
