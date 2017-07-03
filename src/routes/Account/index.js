4738b14de32b70b9c3b2944762ce635262129868
Brandon Jones <brandon@radroot.com>
2017-07-02 21:46:20 -0400

    Remove fetches on route load (components each handle this now)

--- src/routes/Account/index.js
+++ src/routes/Account/index.js
@@ -1,15 +1,10 @@
 import AccountViewContainer from './views/AccountViewContainer'
-import { fetchRooms } from 'store/modules/rooms'
-import { fetchPrefs } from 'store/modules/prefs'
 
 // route definition
 export default function (store) {
   return {
     path: 'account',
     getComponent (nextState, cb) {
-      store.dispatch(fetchRooms())
-      store.dispatch(fetchPrefs())
-
       cb(null, AccountViewContainer)
     }
   }
