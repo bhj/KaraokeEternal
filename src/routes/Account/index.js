d0826e5b0cc7f3b2d768970e52e062fe2d57a0d2
Brandon Jones <brandon@radroot.com>
2017-07-19 22:13:16 -0400

    Fetch prefs on account route load so we can detect firstRun flag

--- src/routes/Account/index.js
+++ src/routes/Account/index.js
@@ -1,10 +1,15 @@
 import AccountViewContainer from './views/AccountViewContainer'
+import { fetchPrefs } from 'store/modules/prefs'
 
 // route definition
 export default function (store) {
   return {
     path: 'account',
     getComponent (nextState, cb) {
+      // Prefs component fetches prefs when it mounts, but we
+      // need to do it here too to detect the firstRun flag
+      store.dispatch(fetchPrefs())
+
       cb(null, AccountViewContainer)
     }
   }
