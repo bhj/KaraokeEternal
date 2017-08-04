dadd060a7cf45ad64b264447aba67b05b50ebfb2
Brandon Jones <brandon@radroot.com>
2017-08-04 13:14:39 -0400

    Initial rewrite of getLibrary for the new media table

--- server/lib/getLibrary.js
+++ server/lib/getLibrary.js
@@ -3,102 +3,87 @@ const squel = require('squel')
 const debug = require('debug')
 const log = debug('app:library:get')
 
-async function getLibrary (find = {}, providerData = false) {
-  let artists = {
+/**
+ * Gets all artists/songs without detailed media info
+ * (suitable for pushing out to non-admin users)
+ *
+ * @return {[object]}          Results normalized by artist and song
+ */
+async function getLibrary () {
+  const artists = {
     result: [],
     entities: {}
   }
-  let songs = {
+  const songs = {
     result: [],
     entities: {}
   }
 
-  // first query: songs
-  try {
-    const q = squel.select()
-      .field('songId, artistId, title, duration, provider')
-      .field('COUNT(stars.userId) AS stars')
-      .from('songs')
-      .left_join('stars USING(songId)')
-      .group('songId')
-      .order('title')
-
-    // off by default since it requires extra processing
-    if (providerData) {
-      q.field('providerData')
-    }
-
-    // artistId filter
-    if (typeof find.artistId !== 'undefined') {
-      q.where('artistId = ?', find.artistId)
-    }
-
-    // other filters
-    Object.keys(find).map(key => {
-      if (key === 'providerData' && typeof find.providerData === 'object') {
-        Object.keys(find.providerData).map(i => {
-          q.where(`json_extract(providerData, '$.${i}') = ?`, find.providerData[i])
-        })
-      } else if (key !== 'artistId') {
-        q.where(`${key} = ?`, find[key])
-      }
-    })
-
-    // log(q.toString())
-    const { text, values } = q.toParam()
-    const rows = await db.all(text, values)
-
-    // normalize results
-    rows.forEach(function (row) {
-      if (providerData) {
-        row.providerData = JSON.parse(row.providerData)
-      }
-
-      songs.result.push(row.songId)
-      songs.entities[row.songId] = row
-
-      // used in library view as parent/child LUT
-      if (typeof artists.entities[row.artistId] === 'undefined') {
-        artists.entities[row.artistId] = {
-          songIds: [],
-        }
-      }
-
-      artists.entities[row.artistId].songIds.push(row.songId)
-    })
-  } catch (err) {
-    log(err.message)
-    return Promise.reject(err)
-  }
-
-  // second query: artists
+  // First query: artists
   try {
     const q = squel.select()
       .from('artists')
       .order('name')
 
-    // artistId filter
-    if (typeof find.artistId !== 'undefined') {
-      q.where('artistId = ?', find.artistId)
-      delete find.artistId
-    }
-
-    // if other filters are present we'll need to
-    // only include songs returned in first query
-    if (Object.keys(find).length) {
-      q.where('artistId IN ?', songs.result.map(songId => songs.entities[songId].artistId))
-    }
-
     // log(q.toString())
     const { text, values } = q.toParam()
     const rows = await db.all(text, values)
 
     // normalize results
-    rows.forEach(function (row) {
+    for (const row of rows) {
       artists.result.push(row.artistId)
-      // merge with the LUT from first query
-      artists.entities[row.artistId] = Object.assign(artists.entities[row.artistId], row)
-    })
+      artists.entities[row.artistId] = row
+      // prep LUT for songIds
+      artists.entities[row.artistId].songIds = []
+    }
+  } catch (err) {
+    log(err.message)
+    return Promise.reject(err)
+  }
+
+  // Second query: media/songs
+  //
+  // This could probably be improved but writing a single query that properly
+  // returns the right media item while handling enabled/disabled providers,
+  // provider priority, per-song preferred media (with fallback if preferred
+  // media's provider is disabled), etc. proved difficult. At the very least
+  // this query avoids subqueries and should be relatively performant.
+  try {
+    const q = squel.select()
+      .field('media.mediaId, media.songId, media.duration, media.provider, media.isPreferred')
+      .field('songs.artistId, songs.title')
+      .field('COUNT(stars.userId) AS numStars')
+      .from('media')
+      .join('providers ON (providers.name = media.provider)')
+      .join('songs USING(songId)')
+      .left_join('stars USING(songId)')
+      .where('providers.isEnabled = 1')
+      .group('mediaId')
+      .order('providers.priority')
+      .order('songs.title')
+
+    const { text, values } = q.toParam()
+    const rows = await db.all(text, values)
+
+    // normalize and process results
+    for (const row of rows) {
+      if (typeof songs.entities[row.songId] === 'undefined') {
+        // add songId to artist's LUT
+        artists.entities[row.artistId].songIds.push(row.songId)
+
+        songs.result.push(row.songId)
+        songs.entities[row.songId] = row
+        songs.entities[row.songId].numMedia = 1
+      } else if (row.isPreffered === 1) {
+        // The previous query should have returned media ordered by the
+        // correct provider priority, so we only want to overwrite an
+        // exisiting entry if this media's isPreferred flag is set.
+        songs.entities[row.songId] = row
+      }
+
+      // increment song's media item count
+      ++songs.entities[row.songId].numMedia
+    }
   } catch (err) {
     log(err.message)
     return Promise.reject(err)
