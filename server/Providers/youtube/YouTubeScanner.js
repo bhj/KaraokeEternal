43a74c116ecab1c7643f600edbf77b910d674678
Brandon Jones <brandon@radroot.com>
2017-08-28 14:10:44 -0400

    Make YouTube scanner Classier and more cancel-able

--- server/Providers/youtube/YouTubeScanner.js
+++ server/Providers/youtube/YouTubeScanner.js
@@ -10,164 +10,190 @@ class YouTubeScanner extends Scanner {
   constructor (prefs) {
     super()
     this.prefs = prefs
+
+    if (!prefs.apiKey) {
+      throw new Error('Please set your YouTube API key')
+    }
   }
 
   async scan () {
+    let items = []
     let validIds = [] // mediaIds for cleanup
 
-    if (!this.prefs.apiKey) {
-      throw new Error('Please set your YouTube API key')
-    }
+    // list all videos from all channels
+    for (const channel of this.prefs.channels) {
+      if (this.isCanceling) {
+        break
+      }
 
-    for (const name of this.prefs.channels) {
-      let items
-
-      this.emitStatus(`Getting videos by "${name}"`, 0)
+      this.emitStatus(`Listing "${channel}"`, 0)
 
       try {
-        items = await getPlaylistItems(name, this.prefs.apiKey)
+        const res = await this.getPlaylistItems(channel)
+        items = items.concat(res)
       } catch (err) {
         log('  => %s', err)
       }
-
-      for (const item of items) {
-        try {
-          const mediaId = await process(item)
-          validIds.push(mediaId)
-        } catch (err) {
-          log(err.message)
-        }
-      } // end for
-
-      if (this.isCanceling) {
-        log('Canceling scan (user requested)')
-        break
-      }
     } // end for
-
-    return this.emitDone()
-  }
-}
-
-module.exports = YouTubeScanner
-
-async function process (item) {
-  log('processing: %s', JSON.stringify({
-    title: item.snippet.title,
-    videoId: item.id,
-    duration: item.contentDetails.duration,
-  }))
-
-  // is video already in the db?
-  // try {
-  //   let res = await getLibrary({ providerData: { videoId: item.id } })
-  //   // @todo: check mtime and title for updates
-  //   if (res.songs.result.length) {
-  //     log('song is in library (same videoId)')
-  //     stats.ok++
-  //     return Promise.resolve(res.songs.result[0])
-  //   }
-  // } catch (err) {
-  //   log(err.message)
-  //   return Promise.reject(err)
-  // }
-
-  // new song
-  const { artist, title } = parseArtistTitle(item.snippet.title)
-
-  if (!artist || !title) {
-    log(' => skipping: couldn\'t parse artist/title from video title')
-    return Promise.reject(new Error('couldn\'t parse artist/title'))
-  }
-
-  const song = {
-    artist,
-    title,
-    provider: 'youtube',
-    duration: toSeconds(parse(item.contentDetails.duration)),
-    providerData: {
-      videoId: item.id,
-      username: item.username,
-      publishedAt: item.snippet.publishedAt,
-    }
-  }
-
-  try {
-    const mediaId = await Media.add(song)
-    return Promise.resolve(mediaId)
-  } catch (err) {
-    log(err.message)
-    return Promise.reject(err)
-  }
-}
-
-async function getPlaylistItems (username, key) {
-  let api = getApi(key)
-  let playlistId, playlist
-  let items = []
-
-  // get channel/playlist info for youtube user
-  try {
-    let res = await api(`channels?forUsername=${username}&part=contentDetails`)
-    playlistId = res.items[0].contentDetails.relatedPlaylists.uploads
-
-    if (typeof playlistId !== 'string') {
-      throw new Error('invalid playlist id')
-    }
-  } catch (err) {
-    log(err.message)
-    return Promise.reject(err)
-  }
-
-  // get playlist page by page
-  do {
-    let url = `playlistItems?playlistId=${playlistId}&maxResults=50&part=snippet`
-
-    if (playlist && playlist.nextPageToken) {
-      url += '&pageToken=' + playlist.nextPageToken
-    }
-
-    try {
-      playlist = await api(url)
-    } catch (err) {
-      log(err.message)
-      return Promise.reject(err)
-    }
-
-    // get durations for playlist items
-    let videoIds = playlist.items.map(item => item.snippet.resourceId.videoId)
-    let details
-
-    try {
-      details = await api(`videos?part=contentDetails&id=` + videoIds.join(','))
-    } catch (err) {
-      log(err.message)
-      return Promise.reject(err)
-    }
-
-    // merge snippet and contentDetails data into one
-    // object and add it to our final array of items
-    playlist.items.forEach((item, i) => {
-      items.push(Object.assign({
-        username,
-      }, playlist.items[i], details.items[i]))
-    })
-
-    log('got %s of %s items', items.length, playlist.pageInfo.totalResults)
 
     if (this.isCanceling) {
       log('Canceling scan (user requested)')
       return this.emitDone()
     }
-  } while (playlist.nextPageToken)
 
-  return items
-}
+    for (let i = 0; i < items.length; i++) {
+      this.emitStatus(`Processing videos (${i} of ${items.length})`, (i / items.length) * 100)
 
-function getApi (key) {
-  return async function (params) {
+      try {
+        const mediaId = await process(items[i])
+        validIds.push(mediaId)
+      } catch (err) {
+        log(err.message)
+      }
+    } // end for
+
+    return this.emitDone()
+  }
+
+  /**
+   * List all videos in a channel (using its "uploads" playlist)
+   *
+   * @param  {string}  channel YT channel or username
+   * @return {Promise} array of video items
+   */
+  async getPlaylistItems (channel) {
+    let playlistId, playlist
+    let items = []
+
+    // get channel/playlist info for youtube user
+    try {
+      let res = await this.callApi(`channels?forUsername=${channel}&part=contentDetails`)
+      playlistId = res.items[0].contentDetails.relatedPlaylists.uploads
+
+      if (typeof playlistId !== 'string') {
+        throw new Error('invalid playlist id')
+      }
+    } catch (err) {
+      log(err.message)
+      return Promise.reject(err)
+    }
+
+    if (this.isCanceling) {
+      return
+    }
+
+    // get playlist page by page
+    do {
+      let url = `playlistItems?playlistId=${playlistId}&maxResults=50&part=snippet`
+
+      if (playlist && playlist.nextPageToken) {
+        url += '&pageToken=' + playlist.nextPageToken
+      }
+
+      try {
+        playlist = await this.callApi(url)
+      } catch (err) {
+        log(err.message)
+        return Promise.reject(err)
+      }
+
+      // get durations for playlist items
+      let videoIds = playlist.items.map(item => item.snippet.resourceId.videoId)
+      let details
+
+      try {
+        details = await this.callApi(`videos?part=contentDetails&id=` + videoIds.join(','))
+      } catch (err) {
+        log(err.message)
+        return Promise.reject(err)
+      }
+
+      if (this.isCanceling) {
+        break
+      }
+
+      // merge snippet and contentDetails data into one
+      // object and add it to our final array of items
+      playlist.items.forEach((item, i) => {
+        items.push(Object.assign({
+          channel,
+        }, playlist.items[i], details.items[i]))
+      })
+
+      this.emitStatus(`Listing "${channel}" (${items.length} of ${playlist.pageInfo.totalResults})`,
+        (items.length / playlist.pageInfo.totalResults) * 100)
+
+      log('got %s of %s items', items.length, playlist.pageInfo.totalResults)
+    } while (playlist.nextPageToken && !this.isCanceling)
+
+    return items
+  }
+
+  /**
+   * Process a video item
+   *
+   * @param  {object} item YT composite (snippet + details) video item
+   * @return {number} mediaId
+   */
+  async process (item) {
+    log('processing: %s', JSON.stringify({
+      title: item.snippet.title,
+      videoId: item.id,
+      duration: item.contentDetails.duration,
+    }))
+
+    // is video already in the db?
+    // try {
+    //   let res = await getLibrary({ providerData: { videoId: item.id } })
+    //   // @todo: check mtime and title for updates
+    //   if (res.songs.result.length) {
+    //     log('song is in library (same videoId)')
+    //     stats.ok++
+    //     return Promise.resolve(res.songs.result[0])
+    //   }
+    // } catch (err) {
+    //   log(err.message)
+    //   return Promise.reject(err)
+    // }
+
+    // new song
+    const { artist, title } = parseArtistTitle(item.snippet.title)
+
+    if (!artist || !title) {
+      log(' => skipping: couldn\'t parse artist/title from video title')
+      return Promise.reject(new Error('couldn\'t parse artist/title'))
+    }
+
+    const song = {
+      artist,
+      title,
+      provider: 'youtube',
+      duration: toSeconds(parse(item.contentDetails.duration)),
+      providerData: {
+        videoId: item.id,
+        channel: item.username,
+        publishedAt: item.snippet.publishedAt,
+      }
+    }
+
+    try {
+      const mediaId = await Media.add(song)
+      return Promise.resolve(mediaId)
+    } catch (err) {
+      log(err.message)
+      return Promise.reject(err)
+    }
+  }
+
+  /**
+  * Make an API call
+  *
+  * @param  {string}  params Appended to the base URL
+  * @return {Promise}        Decoded JSON response
+  */
+  async callApi (params) {
     const API = 'https://www.googleapis.com/youtube/v3/'
-    const url = API + params + `&key=${key}`
+    const url = API + params + `&key=${this.prefs.apiKey}`
     log('request: %s', API + params.substr(0, params.indexOf('?')))
 
     try {
@@ -180,6 +206,8 @@ function getApi (key) {
     }
   }
 }
+
+module.exports = YouTubeScanner
 
 // function parseArtistTitle (str) {
 //   let title, artist
