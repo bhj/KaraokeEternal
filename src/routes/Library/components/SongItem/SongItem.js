c4d300f89a34182ae79ae2481e077898a5ee5e2c
Brandon Jones <brandon@radroot.com>
2017-12-03 20:51:56 -0500

    Swipeable songItem initial implementation

--- src/routes/Library/components/SongItem/SongItem.js
+++ src/routes/Library/components/SongItem/SongItem.js
@@ -1,43 +1,79 @@
 import PropTypes from 'prop-types'
 import React from 'react'
+import Swipeable from 'react-swipeable'
 import Icon from 'components/Icon'
 import './SongItem.css'
 
-export const SongItem = (props) => (
-  <div style={props.style} styleName={'container' + (props.isQueued ? ' isQueued' : '')}>
-    <div styleName='duration'>
-      {toMMSS(props.duration)}
-    </div>
+const BTN_WIDTH = 44
 
-    <div onClick={props.onSongClick} styleName='title'>
-      {props.title}
-      {props.numMedia > 1 &&
-        <i> ({props.numMedia})</i>
-      }
-    </div>
+export default class SongItem extends React.Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    duration: PropTypes.number.isRequired,
+    style: PropTypes.object,
+    onSongClick: PropTypes.func.isRequired,
+    onSongStarClick: PropTypes.func.isRequired,
+    isQueued: PropTypes.bool.isRequired,
+    isStarred: PropTypes.bool.isRequired,
+    numStars: PropTypes.number.isRequired,
+    numMedia: PropTypes.number.isRequired,
+  }
 
-    <div onClick={props.onSongStarClick} styleName='star'>
-      <Icon size={44} icon={props.isStarred ? 'STAR_FULL' : 'STAR_EMPTY'}
-        styleName={props.isStarred ? 'starIconFull' : 'starIcon'}
-      />
-      <div styleName={props.isStarred ? 'numStarsFull' : 'numStars'}>{props.numStars}</div>
-    </div>
-  </div>
-)
+  state = {
+    expanded: false,
+  }
 
-SongItem.propTypes = {
-  title: PropTypes.string.isRequired,
-  duration: PropTypes.number.isRequired,
-  style: PropTypes.object,
-  onSongClick: PropTypes.func.isRequired,
-  onSongStarClick: PropTypes.func.isRequired,
-  isQueued: PropTypes.bool.isRequired,
-  isStarred: PropTypes.bool.isRequired,
-  numStars: PropTypes.number.isRequired,
-  numMedia: PropTypes.number.isRequired,
+  handleSwipingLeft = (e, delta) => {
+    this.setState({ expanded: true })
+  }
+
+  handleSwipingRight = (e, delta) => {
+    this.setState({ expanded: false })
+  }
+
+  render () {
+    const { props } = this
+
+    return (
+      <Swipeable
+        onSwipingLeft={this.handleSwipingLeft}
+        onSwipingRight={this.handleSwipingRight}
+        preventDefaultTouchmoveEvent
+        style={props.style}
+        styleName={'container' + (props.isQueued ? ' isQueued' : '')}
+      >
+        <div styleName='duration'>
+          {toMMSS(props.duration)}
+        </div>
+
+        <div onClick={props.onSongClick} styleName='title'>
+          {props.title}
+          {props.numMedia > 1 &&
+            <i> ({props.numMedia})</i>
+          }
+        </div>
+
+        <div style={{ width: this.state.expanded ? BTN_WIDTH * 4 +'px' : BTN_WIDTH + 'px' }} styleName='btnContainer'>
+          <div onClick={props.onSongStarClick} styleName='button'>
+            <Icon size={44} icon={props.isStarred ? 'STAR_FULL' : 'STAR_EMPTY'}
+              styleName={props.isStarred ? 'starIconFull' : 'starIcon'}
+            />
+            <div styleName={props.isStarred ? 'numStarsFull' : 'numStars'}>{props.numStars}</div>
+          </div>
+          <div onClick={props.onSongStarClick} styleName='button'>
+            <Icon size={44} icon='MORE_HORIZ' />
+          </div>
+          <div onClick={props.onSongStarClick} styleName='button'>
+            <Icon size={44} icon='MORE_HORIZ' />
+          </div>
+          <div onClick={props.onSongStarClick} styleName='button'>
+            <Icon size={44} icon='MORE_HORIZ' />
+          </div>
+        </div>
+      </Swipeable>
+    )
+  }
 }
-
-export default SongItem
 
 // convert seconds to mm:ss
 function toMMSS (duration) {
