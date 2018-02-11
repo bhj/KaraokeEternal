d3a350cf569e69959f0cf700be4ccfb38c9b57f1
Brandon Jones <brandon@radroot.com>
2018-02-11 01:45:23 -0500

    Try not lowercasing the rest of the word

--- server/lib/parseMeta.js
+++ server/lib/parseMeta.js
@@ -35,8 +35,8 @@ module.exports = function (str, cfg) {
 }
 
 function titleCase (str) {
-  return str.replace(/\w\S*/g, function (tStr) {
-    return tStr.charAt(0).toUpperCase() + tStr.substr(1).toLowerCase()
+  return str.replace(/\w\S*/g, function (word) {
+    return word.charAt(0).toUpperCase() + word.substr(1)
   })
 }
 
