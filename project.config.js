7133478ca8bc62d41ad4125daf118c4209e277a7
Brandon Jones <brandon@radroot.com>
2018-05-18 00:25:25 -0400

    Revert publicPath change

--- project.config.js
+++ project.config.js
@@ -7,7 +7,7 @@ module.exports = {
   /** Full path to the project's root directory */
   basePath: __dirname,
   /** path for all project assets (relative to website public_html) */
-  publicPath: './',
+  publicPath: '/',
   // http server host: https://nodejs.org/api/net.html#net_server_listen_port_host_backlog_callback
   serverHost: '0.0.0.0',
   // http server port: https://nodejs.org/api/net.html#net_server_listen_port_host_backlog_callback
