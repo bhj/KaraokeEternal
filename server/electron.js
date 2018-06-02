60a039ba6ea31e21d48b226bbda82d993cae04bc
Brandon Jones <brandon@radroot.com>
2018-06-02 13:57:54 -0400

    Fix database path on Windows

--- server/electron.js
+++ server/electron.js
@@ -20,7 +20,7 @@ if (!process.env.NODE_ENV) {
 }
 
 // pass user data path to (non-Electron) forked processes
-process.env.USER_DATA_PATH = app.getPath('userData')
+process.env.USER_DATA_PATH = app.getPath('appData')
 
 // event handlers
 app.on('ready', createWindow)
