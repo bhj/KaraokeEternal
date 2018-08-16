48992018fc9bb6fa38bac553fe2132ce84a50a8a
Brandon Jones <brandon@radroot.com>
2018-08-16 16:28:22 -0400

    Fix path offline detection (don't catch readdir() errors)

--- server/Media/FileScanner/getFiles.js
+++ server/Media/FileScanner/getFiles.js
@@ -1,5 +1,3 @@
-const debug = require('debug')
-const log = debug('app:media:fileScanner:getFiles')
 const { promisify } = require('util')
 const { resolve } = require('path')
 const fs = require('fs')
@@ -7,13 +5,7 @@ const readdir = promisify(fs.readdir)
 const stat = promisify(fs.stat)
 
 async function getFiles (dir, extra) {
-  let list = []
-
-  try {
-    list = await readdir(dir)
-  } catch (err) {
-    log(err)
-  }
+  const list = await readdir(dir)
 
   const files = await Promise.all(list.map(async (item) => {
     const file = resolve(dir, item)
