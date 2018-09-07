cd89b6d9cd347ba70d3e439bb49b49da13040481
Brandon Jones <brandon@radroot.com>
2018-09-07 15:20:54 -0400

    Prevent duplicate stars in db; rename indexes

--- server/Library/socket.js
+++ server/Library/socket.js
@@ -12,25 +12,13 @@ const {
 const ACTION_HANDLERS = {
   [USER_SONG_STAR]: async (sock, { payload }, acknowledge) => {
     // @TODO use upsert
-    // already starred?
-    const q = squel.select()
-      .from('starredSongs')
-      .where('userId = ?', sock.user.userId)
-      .where('songId = ?', payload.songId)
+    const q = squel.insert()
+      .into('starredSongs')
+      .set('userId', sock.user.userId)
+      .set('songId', payload.songId)
 
     const { text, values } = q.toParam()
-    const res = await db.get(text, values)
-
-    // not starred? then we need to!
-    if (!res) {
-      const q = squel.insert()
-        .into('starredSongs')
-        .set('userId', sock.user.userId)
-        .set('songId', payload.songId)
-
-      const { text, values } = q.toParam()
-      await db.run(text, values)
-    }
+    await db.run(text, values)
 
     // success
     acknowledge({ type: USER_SONG_STAR + _SUCCESS })
--- server/lib/db/001-initial-schema.sql
+++ server/lib/db/001-initial-schema.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS "artists" (
   "name" text NOT NULL COLLATE NOCASE
 );
 
-CREATE UNIQUE INDEX IF NOT EXISTS name ON "artists" ("name" ASC);
+CREATE UNIQUE INDEX IF NOT EXISTS idxName ON "artists" ("name" ASC);
 
 CREATE TABLE IF NOT EXISTS "media" (
   "mediaId" integer PRIMARY KEY AUTOINCREMENT NOT NULL,
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS "media" (
   "dateUpdated" integer NOT NULL DEFAULT(0)
 );
 
-CREATE INDEX IF NOT EXISTS songId ON "media" ("songId" ASC);
+CREATE INDEX IF NOT EXISTS idxSong ON "media" ("songId" ASC);
 
 CREATE TABLE IF NOT EXISTS "paths" (
   "pathId" integer PRIMARY KEY AUTOINCREMENT NOT NULL,
@@ -26,7 +26,7 @@ CREATE TABLE IF NOT EXISTS "paths" (
   "priority" integer NOT NULL
 );
 
-CREATE INDEX IF NOT EXISTS pathId ON "paths" ("pathId" ASC);
+CREATE INDEX IF NOT EXISTS idxPath ON "paths" ("pathId" ASC);
 
 CREATE TABLE IF NOT EXISTS "prefs" (
   "key" text PRIMARY KEY NOT NULL,
@@ -42,7 +42,7 @@ CREATE TABLE IF NOT EXISTS "queue" (
   "userId" integer NOT NULL
 );
 
-CREATE INDEX IF NOT EXISTS roomId ON "queue" ("roomId" ASC);
+CREATE INDEX IF NOT EXISTS idxRoom ON "queue" ("roomId" ASC);
 
 CREATE TABLE IF NOT EXISTS "rooms" (
   "roomId" integer PRIMARY KEY AUTOINCREMENT NOT NULL,
@@ -52,29 +52,27 @@ CREATE TABLE IF NOT EXISTS "rooms" (
   "dateCreated" text NOT NULL
 );
 
-CREATE INDEX IF NOT EXISTS status ON "rooms" ("status" ASC);
-
 CREATE TABLE IF NOT EXISTS "songs" (
   "songId" integer PRIMARY KEY AUTOINCREMENT NOT NULL,
   "artistId" integer NOT NULL,
   "title" text NOT NULL COLLATE NOCASE
 );
 
-CREATE INDEX IF NOT EXISTS title ON "songs" ("title" ASC);
+CREATE INDEX IF NOT EXISTS idxTitle ON "songs" ("title" ASC);
 
 CREATE TABLE IF NOT EXISTS "starredArtists" (
   "userId" integer NOT NULL,
   "artistId" integer NOT NULL
 );
 
-CREATE INDEX IF NOT EXISTS userId ON "starredArtists" ("userId" ASC);
+CREATE UNIQUE INDEX IF NOT EXISTS idxUserArtist ON "starredArtists" ("userId" ASC, "artistId" ASC);
 
 CREATE TABLE IF NOT EXISTS "starredSongs" (
   "userId" integer NOT NULL,
   "songId" integer NOT NULL
 );
 
-CREATE INDEX IF NOT EXISTS userId ON "starredSongs" ("userId" ASC);
+CREATE UNIQUE INDEX IF NOT EXISTS idxUserSong ON "starredSongs" ("userId" ASC, "songId" ASC);
 
 CREATE TABLE IF NOT EXISTS "users" (
   "userId" integer PRIMARY KEY AUTOINCREMENT NOT NULL,
@@ -84,7 +82,7 @@ CREATE TABLE IF NOT EXISTS "users" (
   "isAdmin" integer(1) NOT NULL
 );
 
-CREATE UNIQUE INDEX IF NOT EXISTS username ON "users" ("username" ASC);
+CREATE UNIQUE INDEX IF NOT EXISTS idxUsername ON "users" ("username" ASC);
 
 -- Down
 DROP TABLE artists;
