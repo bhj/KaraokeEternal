98d1b94901f5d8ab9a6a87bf43a1f51a8f14cc02
Brandon Jones <brandon@radroot.com>
2018-09-08 21:43:25 -0400

    AlphaPicker waits for touchmove and ignores synthetic mousedown on touch devices
    Fixes accidental "clicks" in the AlphaPicker area when tapping to star a song.

--- src/routes/Library/components/AlphaPicker/AlphaPicker.js
+++ src/routes/Library/components/AlphaPicker/AlphaPicker.js
@@ -6,6 +6,7 @@ import './AlphaPicker.css'
 class AlphaPicker extends React.Component {
   alphabet = '#ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
   state = {
+    isTouchCapable: false,
     isTouching: false,
     char: null,
     y: null,
@@ -16,7 +17,7 @@ class AlphaPicker extends React.Component {
       <div
         styleName='container'
         style={{ height: this.props.height, top: this.props.top }}
-        onTouchStart={this.handleTouch}
+        onTouchStart={this.handleTouchStart}
         onTouchMove={this.handleTouch}
         onTouchEnd={this.handleTouchEnd}
         onMouseDown={this.handleTouch}
@@ -37,14 +38,26 @@ class AlphaPicker extends React.Component {
     )
   }
 
+  handleTouchStart = (e) => {
+    this.setState({
+      isTouchCapable: true,
+      isTouching: true,
+    })
+  }
+
   handleTouch = (e) => {
     e.preventDefault()
+
+    if (e.type === 'mousedown' && this.state.isTouchCapable) {
+      // browser supports touch events but we got a mouse click;
+      // ignore since on touch devices we want to wait for movement
+      return
+    }
 
     const y = (e.targetTouches ? e.targetTouches[0].clientY : e.clientY)
     const char = this.alphabet[Math.floor(((y - this.props.top) / this.props.height) * this.alphabet.length)]
 
     this.setState({
-      isTouching: true,
       char,
       y: y - this.props.top,
     })
