e92d27cff15fcfa5adab19f384a94e6cc3655c25
Brandon Jones <brandon@radroot.com>
2018-09-25 15:42:25 -0400

    Hide a few account update fields until required

--- src/routes/Account/components/Account/AccountForm/AccountForm.js
+++ src/routes/Account/components/Account/AccountForm/AccountForm.js
@@ -10,6 +10,11 @@ export default class AccountForm extends Component {
     onSubmitClick: PropTypes.func.isRequired,
   }
 
+  state = {
+    showConfirmPassword: !this.props.user,
+    showCurrentPassword: false,
+  }
+
   handleRoomSelectRef = r => { this.roomSelect = r }
 
   render () {
@@ -20,40 +25,48 @@ export default class AccountForm extends Component {
         <input type='text'
           autoFocus={!user}
           defaultValue={user ? user.username : ''}
+          onChange={this.updateVisible}
           placeholder={user ? 'new username or email' : 'username or email'}
           ref={r => { this.username = r }}
         />
+
         <input type='password'
+          onChange={this.updateVisible}
           placeholder={user ? 'new password (optional)' : 'password'}
           ref={r => { this.newPassword = r }}
         />
-        <input type='password'
-          placeholder={user ? 'new password confirm' : 'confirm password'}
-          ref={r => { this.newPasswordConfirm = r }}
-        />
 
-        {user &&
+        {this.state.showConfirmPassword &&
           <input type='password'
-            placeholder='current password'
-            ref={r => { this.curPassword = r }}
-            style={{ marginBottom: 0 }}
+            placeholder={user ? 'new password confirm' : 'confirm password'}
+            ref={r => { this.newPasswordConfirm = r }}
           />
         }
 
         <br />
-
         <input type='text'
           defaultValue={user ? user.name : ''}
+          onChange={this.updateVisible}
           placeholder='name (public)'
           ref={r => { this.name = r }}
         />
+
+        {this.state.showCurrentPassword &&
+          <>
+            <br />
+            <input type='password'
+              placeholder='current password'
+              ref={r => { this.curPassword = r }}
+              style={{ marginBottom: 0 }}
+            />
+          </>
+        }
 
         {showRoom &&
           <RoomSelect onRef={this.handleRoomSelectRef} />
         }
 
         <br />
-
         <button onClick={this.handleSubmit} className='primary'>
           {user ? 'Update Account' : 'Create Account'}
         </button>
@@ -68,8 +81,8 @@ export default class AccountForm extends Component {
       name: this.name.value.trim(),
       username: this.username.value.trim(),
       password: this.curPassword ? this.curPassword.value : null,
-      newPassword: this.newPassword.value,
-      newPasswordConfirm: this.newPasswordConfirm.value
+      newPassword: this.newPassword ? this.newPassword.value : null,
+      newPasswordConfirm: this.newPasswordConfirm ? this.newPasswordConfirm.value : null,
     }
 
     if (this.roomSelect) {
@@ -77,5 +90,16 @@ export default class AccountForm extends Component {
     }
 
     this.props.onSubmitClick(data)
+  }
+
+  updateVisible = () => {
+    if (!this.props.user) return
+
+    this.setState({
+      showConfirmPassword: !!this.newPassword.value,
+      showCurrentPassword: !!this.newPassword.value ||
+        this.username.value !== this.props.user.username ||
+        this.name.value !== this.props.user.name,
+    })
   }
 }
