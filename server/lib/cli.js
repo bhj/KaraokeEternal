5ac99813ccf0195518625dcd9425aed702bab2c0
Brandon Jones <brandon@radroot.com>
2018-11-05 17:06:07 -0500

    Fix the real issue with setting log level 0

--- server/lib/cli.js
+++ server/lib/cli.js
@@ -15,7 +15,6 @@ const argv = require('yargs')
     alias: 'loglevel',
     describe: 'Log file level (0=off, 1=error, 2=warn, 3=info, 4=verbose, 5=debug) (default=2)',
     requiresArg: true,
-    // type: 'number' will coerce/lose zero (see https://github.com/yargs/yargs/issues/963)
   })
   .option('p', {
     alias: 'port',
@@ -26,8 +25,8 @@ const argv = require('yargs')
   .argv
 
 const options = {
-  l: 'KF_LOG_LEVEL',
-  p: 'KF_SERVER_PORT',
+  loglevel: 'KF_LOG_LEVEL',
+  port: 'KF_SERVER_PORT',
 }
 
 // Sets environment variables for the current process based on CLI args.
@@ -37,7 +36,7 @@ function computeEnv () {
 
   // an option set via CLI takes precendence over the environment variable
   Object.keys(options).forEach(key => {
-    if (argv[key]) {
+    if (typeof argv[key] !== 'undefined') {
       env[options[key]] = argv[key]
       process.env[options[key]] = argv[key]
     } else if (process.env[options[key]]) {
