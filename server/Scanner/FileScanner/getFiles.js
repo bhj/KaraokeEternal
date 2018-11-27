1a99135f8634bad38431604966cff261382a3842
Brandon Jones <brandon@radroot.com>
2018-11-27 11:20:04 -0500

    Don't list folders starting with period

--- server/Scanner/FileScanner/getFiles.js
+++ server/Scanner/FileScanner/getFiles.js
@@ -7,10 +7,12 @@ const stat = promisify(fs.stat)
 async function getFiles (dir, extra) {
   const list = await readdir(dir)
 
-  const files = await Promise.all(list.map(async (item) => {
-    const file = resolve(dir, item)
-    return (await stat(file)).isDirectory() ? getFiles(file, extra) : { file, ...extra }
-  }))
+  const files = await Promise.all(list
+    .filter(d => !d.startsWith('.'))
+    .map(async (item) => {
+      const file = resolve(dir, item)
+      return (await stat(file)).isDirectory() ? getFiles(file, extra) : { file, ...extra }
+    }))
 
   return files.reduce((a, f) => a.concat(f), [])
 }
