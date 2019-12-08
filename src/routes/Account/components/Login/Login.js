94aea2acf5b58b16876b58f87a5b84435bd5c4bf
Brandon Jones <brandon@radroot.com>
2019-12-07 23:53:19 -0500

    Add logo to login/create account "page"; re-style

--- src/routes/Account/components/Login/Login.js
+++ src/routes/Account/components/Login/Login.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
+import Logo from 'components/Logo'
 import LoginForm from './LoginForm'
 import AccountForm from '../Account/AccountForm'
 import './Login.css'
@@ -14,43 +15,50 @@ export default class Login extends Component {
   }
 
   state = {
-    view: 'login',
+    isCreating: false,
   }
 
   toggleMode = () => this.setState({
-    view: this.state.view === 'login' ? 'create' : 'login'
+    isCreating: !this.state.isCreating,
   })
 
   render () {
     const { isFirstRun, user } = this.props
-    const { view } = this.state
+    const { isCreating } = this.state
 
     return (
       <div styleName='container'>
-        <h1 styleName='title'>Karaoke Forever</h1>
-        <div styleName='content'>
-          {!isFirstRun && view === 'login' &&
-          <>
-            <p>Sign in below. <a onClick={this.toggleMode}>Don&rsquo;t have an account?</a></p>
-            <LoginForm onSubmitClick={this.props.login} />
-          </>
-          }
+        <Logo styleName='logo'/>
 
-          {!isFirstRun && view === 'create' &&
-          <>
-            <p>Create an account below. <a onClick={this.toggleMode}>Already have an account?</a></p>
-            <AccountForm user={user} onSubmitClick={this.props.createAccount} showRoom />
-          </>
-          }
+        {!isFirstRun && !isCreating &&
+        <>
+          <div styleName='heading'>
+            <h1>Sign in</h1>
+            <span><a onClick={this.toggleMode}>Don&rsquo;t have an account?</a></span>
+          </div>
+          <LoginForm onSubmitClick={this.props.login} />
+        </>
+        }
 
-          {isFirstRun &&
-          <>
-            <p>Welcome! Create your <b>admin</b> account below to get started.
-            All accounts are stored locally on your server.</p>
-            <AccountForm user={user} onSubmitClick={this.props.createAccount} showRoom={false} />
-          </>
-          }
-        </div>
+        {!isFirstRun && isCreating &&
+        <>
+          <div styleName='heading'>
+            <h1>Create account</h1>
+            <span><a onClick={this.toggleMode}>Already have an account?</a></span>
+          </div>
+          <AccountForm user={user} onSubmitClick={this.props.createAccount} showRoom />
+        </>
+        }
+
+        {isFirstRun &&
+        <>
+          <div styleName='heading'>
+            <h1>Welcome</h1>
+            <span>Create your <b>admin</b> account to get started. All data is stored on your server.</span>
+          </div>
+          <AccountForm user={user} onSubmitClick={this.props.createAccount} showRoom={false} />
+        </>
+        }
       </div>
     )
   }
