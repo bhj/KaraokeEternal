33a4a8aa9d9c154ccc87ca0ebae113784afc5423
Brandon Jones <brandon@radroot.com>
2020-02-02 22:41:55 -0500

    Add default ReplayGain pref to schema

--- server/lib/schemas/002-replaygain.sql
+++ server/lib/schemas/002-replaygain.sql
@@ -2,4 +2,6 @@
 ALTER TABLE "media" ADD COLUMN "rgTrackGain" REAL;
 ALTER TABLE "media" ADD COLUMN "rgTrackPeak" REAL;
 
+INSERT INTO prefs (key,data) VALUES ('isReplayGainEnabled','false');
+
 -- Down
