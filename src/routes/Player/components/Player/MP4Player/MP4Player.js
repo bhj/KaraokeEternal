a96801db3585b2ec7314438c7da89d7917dc8684
Brandon Jones <brandon@radroot.com>
2020-07-08 02:26:57 -0400

    First keying implementation using a single canvas

--- src/routes/Player/components/Player/MP4Player/MP4Player.js
+++ src/routes/Player/components/Player/MP4Player/MP4Player.js
@@ -18,10 +18,21 @@ class MP4Player extends React.Component {
     onStatus: PropTypes.func.isRequired,
   }
 
-  video = React.createRef()
+  canvas = React.createRef()
+  video = document.createElement('video')
+  frameId = null
 
   componentDidMount () {
-    this.props.onAudioElement(this.video.current)
+    this.video.oncanplaythrough = this.updateIsPlaying
+    this.video.onended = this.props.onEnd
+    this.video.onerror = this.handleError
+    this.video.onloadstart = this.props.onLoad
+    this.video.onplay = this.handlePlay
+    this.video.ontimeupdate = this.handleTimeUpdate
+    this.video.preload = 'auto'
+
+    this.props.onAudioElement(this.video)
+    this.canvasCtx = this.canvas.current.getContext('2d')
     this.updateSources()
   }
 
@@ -39,32 +50,48 @@ class MP4Player extends React.Component {
     const { width, height } = this.props
 
     return (
-      <video styleName='video'
-        preload='auto'
+      <canvas
         width={width}
         height={height}
-        onCanPlayThrough={this.updateIsPlaying}
-        onEnded={this.props.onEnd}
-        onError={this.handleError}
-        onLoadStart={this.props.onLoad}
-        onPlay={this.handlePlay}
-        onTimeUpdate={this.handleTimeUpdate}
-        ref={this.video}
+        ref={this.canvas}
       />
     )
   }
 
+  updateFrame = () => {
+    this.frameId = requestAnimationFrame(this.updateFrame)
+
+    this.canvasCtx.drawImage(this.video, 0, 0, this.video.videoWidth, this.video.videoHeight)
+
+    const frame = this.canvasCtx.getImageData(0, 0, this.video.videoWidth, this.video.videoHeight)
+    const l = frame.data.length / 4
+
+    for (let i = 0; i < l; i++) {
+      const r = frame.data[i * 4 + 0]
+      const g = frame.data[i * 4 + 1]
+      const b = frame.data[i * 4 + 2]
+
+      if (g < 20 && r < 20 && b < 20) {
+        frame.data[i * 4 + 3] = 0
+      }
+    }
+
+    this.canvasCtx.putImageData(frame, 0, 0)
+  }
+
   updateSources = () => {
-    this.video.current.src = `/api/media/${this.props.mediaId}?type=video`
-    this.video.current.load()
+    this.video.src = `/api/media/${this.props.mediaId}?type=video`
+    this.video.load()
   }
 
   updateIsPlaying = () => {
     if (this.props.isPlaying) {
-      this.video.current.play()
+      this.video.play()
         .catch(err => this.props.onError(err.message))
     } else {
-      this.video.current.pause()
+      this.video.pause()
+      cancelAnimationFrame(this.frameId)
+      this.frameId = null
     }
   }
 
@@ -76,11 +103,14 @@ class MP4Player extends React.Component {
     this.props.onError(`${message} (code ${code})`)
   }
 
-  handlePlay = () => this.props.onPlay()
+  handlePlay = () => {
+    this.props.onPlay()
+    this.updateFrame()
+  }
 
   handleTimeUpdate = () => {
     this.props.onStatus({
-      position: this.video.current.currentTime,
+      position: this.video.currentTime,
     })
   }
 }
