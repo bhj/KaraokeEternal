fcbb0b25b600f6bda29375bc1b5a4c6b4e434b60
Brandon Jones <brandon@radroot.com>
2020-08-24 16:53:30 -0400

    Don't use yargs' default --version handler
    Fixes -v not working

--- server/lib/cli.js
+++ server/lib/cli.js
@@ -1,9 +1,12 @@
+let _app
 const options = {
   loglevel: 'KF_LOG_LEVEL',
   port: 'KF_SERVER_PORT',
 }
 
 const yargs = require('yargs')
+  .version(false) // disable default handler
+  .command('scanonly', 'Run a media scan on server startup, then exit when finished')
   .option('l', {
     alias: 'loglevel',
     describe: 'Log file level (0=off, 1=error, 2=warn, 3=info, 4=verbose, 5=debug) (default=2)',
@@ -14,19 +17,25 @@ const yargs = require('yargs')
     describe: 'Web server port (default=0/auto)',
     requiresArg: true,
   })
+  .option('v', {
+    alias: 'version',
+    describe: 'Output the Karaoke Forever Server version and exit',
+  })
 
 let argv = yargs.argv
 
 if (process.versions.electron) {
-  const app = require('electron').app
-  yargs.version(app.getVersion())
+  _app = require('electron').app
 
   // see https://github.com/yargs/yargs/blob/master/docs/api.md#argv
-  if (app.isPackaged) {
+  if (_app.isPackaged) {
     argv = yargs.parse(process.argv.slice(1))
   }
-} else {
-  yargs.version(process.env.npm_package_version)
+}
+
+if (argv.version) {
+  console.log(_app ? _app.getVersion() : process.env.npm_package_version)
+  process.exit(0)
 }
 
 // Sets environment variables for the current process based on CLI args.
@@ -34,7 +43,7 @@ if (process.versions.electron) {
 function computeEnv () {
   const env = { NODE_ENV: process.env.NODE_ENV }
 
-  // an option set via CLI takes precendence over the environment variable
+  // options set via CLI take precendence over env vars
   Object.keys(options).forEach(key => {
     if (typeof argv[key] !== 'undefined') {
       env[options[key]] = argv[key]
