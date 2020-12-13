14aea66ed24e152c4c1127abac8d0eb2b4124895
Brandon Jones <brandon@radroot.com>
2020-12-12 22:41:30 -0500

    Hookify PathChooser
    This could be optimized a bit more

--- src/routes/Account/components/Prefs/PathPrefs/PathChooser/PathChooser.js
+++ src/routes/Account/components/Prefs/PathPrefs/PathChooser/PathChooser.js
@@ -1,80 +1,78 @@
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import PathItem from './PathItem'
 import Modal from 'components/Modal'
 import HttpApi from 'lib/HttpApi'
 import './PathChooser.css'
+const api = new HttpApi('prefs/path')
 
-export default class PathChooser extends React.Component {
-  static propTypes = {
-    isVisible: PropTypes.bool.isRequired,
-    onChoose: PropTypes.func.isRequired,
-    onCancel: PropTypes.func.isRequired,
-  }
-
-  state = {
+const PathChooser = props => {
+  const listRef = useRef()
+  const [pathInfo, setPathInfo] = useState({
     current: null,
     parent: null,
     children: [],
-  }
+  })
 
-  api = new HttpApi('prefs/path')
-  list = React.createRef()
+  const handleChoose = useCallback(() => props.onChoose(pathInfo.current), [pathInfo.current])
+  const ls = useCallback(dir => {
+    api('GET', `/ls?dir=${encodeURIComponent(dir)}`)
+      .then(res => setPathInfo(res))
+      .catch(err => alert(err))
+  })
 
-  getListing = (dir) => {
-    this.api('GET', `/ls?dir=${encodeURIComponent(dir)}`)
-      .then(res => this.setState(res))
-      .catch(err => {
-        alert(err)
-      })
-  }
+  // get initial list when chooser first becomes visible
+  useEffect(() => {
+    if (props.isVisible) ls(pathInfo.current || '.')
+  }, [props.isVisible])
 
-  componentDidUpdate (prevProps, prevState) {
-    if (this.props.isVisible && !prevProps.isVisible) {
-      this.getListing(this.state.current || '.')
-    }
+  // scroll to top when changing dirs
+  useEffect(() => {
+    if (listRef.current) listRef.current.scrollTop = 0
+  }, [pathInfo.current])
 
-    if (this.state.current !== prevState.current) {
-      this.list.current.scrollTop = 0
-    }
-  }
-
-  render () {
-    return (
-      <Modal
-        isVisible={this.props.isVisible}
-        onClose={this.props.onCancel}
-        title='Add Folder'
-        style={{
-          width: '100%',
-          height: '100%',
-        }}
-      >
-        <div styleName='container'>
-          <div styleName='folderCurrent'>
-            {this.state.current || '\u00a0'}
-          </div>
-
-          <div styleName='folderList' ref={this.list}>
-            {this.state.parent !== false &&
-              <strong><PathItem path={'..'} onSelect={() => this.getListing(this.state.parent)} /></strong>
-            }
-
-            {this.state.children.map((item, i) =>
-              <PathItem key={i} path={item.label} onSelect={() => this.getListing(item.path)} />
-            )}
-          </div>
-
-          <div style={{ display: 'flex' }}>
-            <button styleName='submit' className='primary' onClick={() => this.props.onChoose(this.state.current)}>
-                Add Folder
-            </button>
-            <button styleName='cancel' onClick={this.props.onCancel}>
-              Cancel
-            </button>
-          </div>
+  return (
+    <Modal
+      isVisible={props.isVisible}
+      onClose={props.onCancel}
+      title='Add Folder'
+      style={{
+        width: '100%',
+        height: '100%',
+      }}
+    >
+      <div styleName='container'>
+        <div styleName='folderCurrent'>
+          {pathInfo.current || '\u00a0'}
         </div>
-      </Modal>
-    )
-  }
+
+        <div styleName='folderList' ref={listRef}>
+          {pathInfo.parent !== false &&
+            <strong><PathItem path={'..'} onSelect={() => ls(pathInfo.parent)} /></strong>
+          }
+
+          {pathInfo.children.map((item, i) =>
+            <PathItem key={i} path={item.label} onSelect={() => ls(item.path)} />
+          )}
+        </div>
+
+        <div style={{ display: 'flex' }}>
+          <button styleName='submit' className='primary' onClick={handleChoose}>
+              Add Folder
+          </button>
+          <button styleName='cancel' onClick={props.onCancel}>
+            Cancel
+          </button>
+        </div>
+      </div>
+    </Modal>
+  )
 }
+
+PathChooser.propTypes = {
+  isVisible: PropTypes.bool.isRequired,
+  onChoose: PropTypes.func.isRequired,
+  onCancel: PropTypes.func.isRequired,
+}
+
+export default PathChooser
