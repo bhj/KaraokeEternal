0b1d45d02743f386b183f2e1d4046fb23073d18e
Brandon Jones <brandon@radroot.com>
2020-12-19 21:54:53 -0500

    Fix redundant export

--- src/components/Header/PlaybackCtrl/NoPlayer/NoPlayer.js
+++ src/components/Header/PlaybackCtrl/NoPlayer/NoPlayer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './NoPlayer.css'
 
-export const NoPlayer = (props) => (
+const NoPlayer = (props) => (
   <div styleName='container'>
     <p styleName='msg'>
       No player in room (<Link to='/player'>Start Player</Link>)
