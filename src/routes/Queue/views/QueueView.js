4753609da475c55ce78efecb108978288e82e4c1
Brandon Jones <brandon@radroot.com>
2021-12-16 16:39:17 -0500

    Ensure current song is in view on first mount only

--- src/routes/Queue/views/QueueView.js
+++ src/routes/Queue/views/QueueView.js
@@ -1,24 +1,42 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useSelector } from 'react-redux'
 import { ensureState } from 'redux-optimistic-ui'
 import { Link } from 'react-router-dom'
+import getRoundRobinQueue from '../selectors/getRoundRobinQueue'
 import QueueList from '../components/QueueList'
 import Spinner from 'components/Spinner'
 import TextOverlay from 'components/TextOverlay'
 import styles from './QueueView.css'
 
+const QUEUE_ITEM_HEIGHT = 92
+
 const QueueView = (props) => {
   const { innerWidth, innerHeight, headerHeight, footerHeight } = useSelector(state => state.ui)
   const isInRoom = useSelector(state => !!state.user.roomId)
-  const queue = useSelector(state => ensureState(state.queue))
+  const isLoading = useSelector(state => ensureState(state.queue).isLoading)
+  const queue = useSelector(getRoundRobinQueue)
+  const queueId = useSelector(state => state.status.queueId)
+  const containerRef = useRef()
+
+  // ensure current song is in view on first mount only
+  useEffect(() => {
+    if (containerRef.current) {
+      const i = queue.result.indexOf(queueId)
+      containerRef.current.scrollTop = QUEUE_ITEM_HEIGHT * i
+    }
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
-    <div className={styles.container} style={{
-      paddingTop: headerHeight,
-      paddingBottom: footerHeight,
-      width: innerWidth,
-      height: innerHeight,
-    }}>
+    <div
+      className={styles.container}
+      ref={containerRef}
+      style={{
+        paddingTop: headerHeight,
+        paddingBottom: footerHeight,
+        width: innerWidth,
+        height: innerHeight,
+      }}
+    >
       {!isInRoom &&
         <TextOverlay>
           <h1>Get a Room!</h1>
@@ -26,18 +44,18 @@ const QueueView = (props) => {
         </TextOverlay>
       }
 
-      {queue.isLoading &&
+      {isLoading &&
         <Spinner />
       }
 
-      {!queue.isLoading && queue.result.length === 0 &&
+      {!isLoading && queue.result.length === 0 &&
         <TextOverlay>
           <h1>Queue Empty</h1>
           <p>Tap a song in the <Link to='/library'>library</Link> to queue it.</p>
         </TextOverlay>
       }
 
-      <QueueList />
+      <QueueList/>
     </div>
   )
 }
