ee36ad36e38d07b225296e1dff7c7c5d01bd1c19
Brandon Jones <brandon@radroot.com>
2021-12-18 16:54:17 -0500

    fix: Don't animate when QueueItems resize

--- src/routes/Queue/components/QueueListAnimator/QueueListAnimator.js
+++ src/routes/Queue/components/QueueListAnimator/QueueListAnimator.js
@@ -32,6 +32,7 @@ const QueueListAnimator = ({ children }) => {
         onExit={handleExit}
         shouldFlip={handleShouldFlip}
         stagger
+        translate
       >
         <div>
           {child}
