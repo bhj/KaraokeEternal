5e5e3f3989ee995eb47bb694c589b5c988203fa3
Brandon Jones <brandon@radroot.com>
2021-12-20 23:32:33 -0500

    fix: Don't count current queue item if it's in history
    This fixes an edge case when 1) the current item finished (or was skipped), 2) isAtQueueEnd was triggered, 3) global playback was paused, and 4) more songs were queued. In that case, the "up next in..." time would incorrectly include any remaining time from the current item.

--- src/routes/Queue/selectors/getWaits.js
+++ src/routes/Queue/selectors/getWaits.js
@@ -1,4 +1,5 @@
 import { createSelector } from 'reselect'
+import getPlayerHistory from './getPlayerHistory'
 import getRoundRobinQueue from './getRoundRobinQueue'
 
 const getPosition = (state) => state.status.position
@@ -7,8 +8,8 @@ const getQueueId = (state) => state.status.queueId
 const getSongs = (state) => state.songs
 
 const getWaits = createSelector(
-  [getQueue, getQueueId, getPosition, getSongs],
-  (queue, queueId, position, songs) => {
+  [getQueue, getQueueId, getPlayerHistory, getPosition, getSongs],
+  (queue, queueId, history, position, songs) => {
     const curIdx = queue.result.indexOf(queueId)
     const waits = {}
     let curWait = 0
@@ -19,8 +20,10 @@ const getWaits = createSelector(
       if (!songs.entities[songId]) return
 
       if (i === curIdx) {
-        // currently playing
-        nextWait = Math.round(songs.entities[songId].duration - position)
+        // if history includes the current item it's already been played
+        if (history.lastIndexOf(queueId) === -1) {
+          nextWait = Math.round(songs.entities[songId].duration - position)
+        }
       } else if (i > curIdx) {
         // upcoming
         curWait += nextWait
