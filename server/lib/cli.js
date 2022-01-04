1046c04c432c43ad9790c4c82a55b79a58136c31
Brandon Jones <brandon@radroot.com>
2022-01-03 23:59:17 -0500

    improve: CLI option and env var consistency

--- server/lib/cli.js
+++ server/lib/cli.js
@@ -28,23 +28,23 @@ const yargs = require('yargs')
     requiresArg: true,
     type: 'string',
   })
-  .option('scannerConsoleLevel', {
-    describe: 'Media scanner console level (default=4)',
+  .option('consoleLevel', {
+    describe: 'Web server console log level (default=4)',
     number: true,
     requiresArg: true,
   })
-  .option('scannerLogLevel', {
-    describe: 'Media scanner log file level (default=3)',
+  .option('logLevel', {
+    describe: 'Web server file log level (default=3)',
     number: true,
     requiresArg: true,
   })
-  .option('serverConsoleLevel', {
-    describe: 'Web server console level (default=4)',
+  .option('scanConsoleLevel', {
+    describe: 'Media scanner console log level (default=4)',
     number: true,
     requiresArg: true,
   })
-  .option('serverLogLevel', {
-    describe: 'Web server log file level (default=3)',
+  .option('scanLogLevel', {
+    describe: 'Media scanner file log level (default=3)',
     number: true,
     requiresArg: true,
   })
@@ -53,7 +53,7 @@ const yargs = require('yargs')
     describe: 'Output the Karaoke Forever Server version and exit',
   })
   .usage('$0')
-  .usage('  Some options use the following numeric levels:')
+  .usage('  Logging options use the following numeric levels:')
   .usage('  0=off, 1=error, 2=warn, 3=info, 4=verbose, 5=debug')
 
 let argv = yargs.argv
@@ -85,8 +85,8 @@ if (argv.rotateKey) {
 const opts = {
   port: 'KF_SERVER_PORT',
   urlPath: 'KF_SERVER_URL_PATH',
-  scannerConsoleLevel: 'KF_SCANNER_CONSOLE_LEVEL',
-  scannerLogLevel: 'KF_SCANNER_LOG_LEVEL',
+  scannerConsoleLevel: 'KF_SERVER_SCAN_CONSOLE_LEVEL',
+  scannerLogLevel: 'KF_SERVER_SCAN_LOG_LEVEL',
   serverConsoleLevel: 'KF_SERVER_CONSOLE_LEVEL',
   serverLogLevel: 'KF_SERVER_LOG_LEVEL',
 }
