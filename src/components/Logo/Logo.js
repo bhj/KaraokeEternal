eb6638cd6d1b8309be292b6cfd41029cb696c193
Brandon Jones <brandon@radroot.com>
2022-04-02 21:56:08 -0400

    feat: New logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,14 +1,12 @@
 import PropTypes from 'prop-types'
 import React from 'react'
-import LogoImage from 'assets/app.png'
 import styles from './Logo.css'
 
 const Logo = (props) => {
   return (
     <div className={`${styles.container} ${props.className}`}>
-      <img src={LogoImage} alt='microphone' className={styles.imgMic}/>
       <h1 className={styles.title}>
-        Karaoke<span className={styles.forever}>Forever</span>
+        Karaoke<span className={styles.eternal}>Eterna<span className={styles.lastChar}>l</span></span>
       </h1>
     </div>
   )
