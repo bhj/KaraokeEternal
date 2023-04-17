5ca0f082dd75a4c07e8deb9fccce76a0ada7ec39
Damian Small <dusaro123@gmail.com>
2023-04-18 09:46:34 +1200

    Add QR Code to player

--- src/routes/Player/views/PlayerView.js
+++ src/routes/Player/views/PlayerView.js
@@ -4,6 +4,8 @@ import { injectReducer } from 'store/reducers'
 import playerReducer from '../modules/player'
 import playerVisualizerReducer from '../modules/playerVisualizer'
 
+import QRCode from "react-qr-code";
+
 import PlayerController from '../components/PlayerController'
 import screenfull from 'screenfull'
 import styles from './PlayerView.css'
@@ -11,6 +13,7 @@ import styles from './PlayerView.css'
 const PlayerView = (props) => {
   const { innerWidth, innerHeight, headerHeight, footerHeight } = useSelector(state => state.ui)
   const viewportHeight = innerHeight - headerHeight - footerHeight
+  const qrValue = document.baseURI;
 
   // @todo: find better place for this?
   const store = useStore()
@@ -30,6 +33,14 @@ const PlayerView = (props) => {
           height: screenfull.isFullscreen ? innerHeight : viewportHeight,
         }}
       >
+        <div className="qrCodeWrap" style={{ height: "auto", margin: "0 auto", maxWidth: 82, width: "100%", position: "absolute", bottom: 16, left: 16, backgroundColor: "#fff", padding: "2px", opacity: 0.8 }}>
+          <QRCode
+            size={256}
+            style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+            value={qrValue}
+            viewBox={`0 0 256 256`}
+          />
+        </div>
         <PlayerController
           width={innerWidth}
           height={screenfull.isFullscreen ? innerHeight : viewportHeight}
